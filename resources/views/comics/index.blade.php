f6532f0ea0b56a0a8cd9f42a1d8b7bbf51c9af73
StefanoMela <stefano.mela25@gmail.com>
2023-10-18 20:07:58 +0200

    feat/ add img

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,9 +5,10 @@
     <h1>{{ $title }}</h1>
     <a href="{{ route('comics.create') }}" class="btn btn-primary">Crea il tuo fumetto!</a>
     @foreach ($comics as $comic)
-    <div class="card my-4">
+    <div class="card my-4 h-100">
       <div class="card-body text-center">
         <h5 class="card-title">{{ $comic->title }}</h5>
+        <img src="{{$comic->thumb}}" class="col-3" alt="">
         <p class="card-text">{{ $comic->series }}</p>
         <p class="card-text">{{ $comic->sale_date }}</p>
         <p class="card-text">{{ $comic->type }}</p>
