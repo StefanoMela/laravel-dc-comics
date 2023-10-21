2204ff68df4d379179b6827aadb355253951d69a
StefanoMela <stefano.mela25@gmail.com>
2023-10-21 13:21:38 +0200

    feat/ add some style

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -9,11 +9,11 @@
       <div class="card-body text-center">
         <h5 class="card-title">{{ $comic->title }}</h5>
         <img src="{{$comic->thumb}}" class="col-3" alt="">
-        <p class="card-text">{{ $comic->series }}</p>
-        <p class="card-text">{{ $comic->sale_date }}</p>
-        <p class="card-text">{{ $comic->type }}</p>
-        <p class="card-text">{{ $comic->price }}</p>
-        <p class="card-text">{{ $comic->description }}</p>
+        <p class="card-text"><strong>Serie: </strong>{{ $comic->series }}</p>
+        <p class="card-text"><strong>Prima Uscita: </strong>{{ $comic->sale_date }}</p>
+        <p class="card-text"><strong>Tipo: </strong>{{ $comic->type }}</p>
+        <p class="card-text"><strong>Prezzo: </strong>{{ $comic->price }}</p>
+        <p class="card-text"><strong>Descrizione: </strong>{{ $comic->description }}</p>
         <a class="btn btn-primary" href="{{route('comics.show', $comic)}}">Dettagli</a>
         <a class="btn btn-warning" href="{{route('comics.edit', $comic)}}">Modifica</a>
         @include('partials._modal')
